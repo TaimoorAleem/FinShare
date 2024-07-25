e9e52545a4dfbac12199d03829c661619505d86c
Taimoor Aleem <taimooraleem01@gmail.com>
2024-07-25 15:27:45 -0400

    Add StockDTO using custom mapping to return stocks.

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,4 +1,5 @@
 ﻿using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -16,7 +17,7 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDTO());
 
             return Ok(stocks);
         }
@@ -31,7 +32,7 @@ namespace api.Controllers
                 return NotFound(); 
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDTO);
         }
     }
 }
---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,22 @@
+﻿using api.DTOs.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDTO ToStockDTO(this Stock stockModel)
+        {
+            return new StockDTO
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
