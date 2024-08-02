c69bc447c71d07ca86a7e729a37bebc842a0da29
Taimoor Aleem <taimooraleem01@gmail.com>
2024-08-01 20:19:05 -0400

    Add Update action in the StockController using UpdateStockRequestDto

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -46,5 +46,26 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDTO());
 
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x =>  x.Id == id);
+
+            if (stockModel == null) return NotFound();
+               
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDTO());
+        }
     }
 }
---
+++ api/DTOs/Stock/UpdateStockRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace api.DTOs.Stock
+{
+    public class UpdateStockRequestDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public decimal Purchase { get; set; }
+
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+    }
+}
