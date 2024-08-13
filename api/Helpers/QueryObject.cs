fdd063ed44e6b3011f017f66155f20dc13deccf1
Taimoor Aleem <taimooraleem01@gmail.com>
2024-08-13 14:21:24 -0400

    Add pagination to retrieve a specified number of stocks per request

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -6,5 +6,7 @@
         public string? CompanyName { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool IsAscending { get; set; } = true;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
